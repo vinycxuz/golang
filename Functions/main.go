d97dd10eaa8b5a2a78fe42719b61c44ec1a754cc
vinycxuz <vinyby@gmail.com>
2025-08-20 20:54:40 -0300

    dia 9, funções

--- Functions/main.go
+++ Functions/main.go
@@ -3,3 +3,13 @@ package Functions
 func main() {
 
 }
+
+/*
+1. Funções
+	Separei o arquivo nesse ponto, pois, função é algo muito específico e uma das partes do coração
+das linguagens de programação. Vale ressaltar que, neste ponto, ainda não sei como é o padrão de organizaão de diretorios
+e packages em Golang. OU seja, estou indo passo a passo, sem pressa e evoluindo gradativamente (com uma velocidade um pouco
+maior).
+
+
+*/
