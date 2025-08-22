292922ad7375d208d0039fa5e8e90feedd425966
vinycxuz <vinyby@gmail.com>
2025-08-21 22:42:41 -0300

    dia 10, funções

--- Functions/main.go
+++ Functions/main.go
@@ -10,6 +10,20 @@ func main() {
 das linguagens de programação. Vale ressaltar que, neste ponto, ainda não sei como é o padrão de organizaão de diretorios
 e packages em Golang. OU seja, estou indo passo a passo, sem pressa e evoluindo gradativamente (com uma velocidade um pouco
 maior).
+O basico é esse:
 
+func something() {}
 
+something()
+
+Quando for retornar 1 ou mais argumentos:
+
+func oneMore(cat1, cat2, cat3 int) (int, int, int) {}
+
+Tambem podemos 'tipar' uma funcão da seguinte forma:
+
+type binOp func( int , int) int
+
+1.2. Parâmetros e retornos
+	Indo mais afundo nesse assunto,
 */
